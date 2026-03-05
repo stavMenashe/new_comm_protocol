b0128ba225a8312928db0a7f623cbc61f1af0cfe
stavMenashe <o523044570@gmail.com>
2026-03-05 20:18:43 +0200

    clear stuff

--- build/test_modules/gnuradio/t_prot/t_modulator.py
+++ build/test_modules/gnuradio/t_prot/t_modulator.py
@@ -31,8 +31,8 @@ class t_modulator(gr.sync_block):
         # take the bits out of the given string.
         bits = ''.join(format(ord(i), '08b') for i in self.text)
         
-        #for i in range(self.t * self.Fs):
-            #self.mod_str.put(-1) # preamble
+        for i in range(self.t * self.Fs):
+            self.mod_str.put(-1) # preamble
 
         for bit in bits:
             if bit == '0':
@@ -59,43 +59,4 @@ class t_modulator(gr.sync_block):
         
         return len(output_items[0])
         
-    def size_of_block(self):
-        size = int((8192 - self.Fs * self.t) / (8 * 3 * self.Fs * self.t))
-        size = size * 8 * 3 * self.Fs * self.t
-        return size
-    
-    def decode_message(self, message):
-        index = 0
-        jumps = self.Fs * self.t
-        curr_ch = ""
-        size = 0
-        count_fails = 0
-        while index + jumps + jumps < len(message):
-            symb0 = message[index]
-            index += jumps
-            symb1 = message[index]
-            index += jumps
-            symb2 = message[index]
-            
-            if symb0 == 1 and symb1 == -1 and symb2 == -1:
-                curr_ch += "0"
-                count_fails = 0
-            elif symb0 == 1 and symb1 == 1 and symb2 == -1:
-                curr_ch += "1"
-                count_fails = 0
-            else:
-                count_fails += 1
-                curr_ch += "0"
-                if count_fails >= 8:
-                    break
-            
-            size += 1
-            if size == 8:
-                dec_val = int(curr_ch, 2)
-                print("-", chr(dec_val), "-", end = "")
-                curr_ch = ""
-                size = 0
-                
-            index += jumps
-    
     
